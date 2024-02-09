f92dda8ec2c36153b94f16e5f1517f7a4ed1c3d0
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-02-09 22:08:36 +0530

    Part-1 EP07.05 Reviewing Lifting Up State Running Day-04

--- PART 1 REACT FUNDAMENTALS [4 PROJECTS]/06 State, Events, and Forms Interactive Components/Travel_List_Project/travel-list/src/App.js
+++ PART 1 REACT FUNDAMENTALS [4 PROJECTS]/06 State, Events, and Forms Interactive Components/Travel_List_Project/travel-list/src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,12 +26,20 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   const handleSubmit = (eventOBJ) => {
     eventOBJ.preventDefault();
+    // if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
   // function handleSubmit(e) {
   //   e.preventDefault();
@@ -37,7 +50,10 @@ function Form() {
       {/* <select option={1}>1</select>
       <select option={2}>2</select>
       <select option={3}>3</select> */}
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -57,11 +73,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item, i) => (
+        {items.map((item, i) => (
           <Item item={item} key={item?.id || i} />
         ))}
       </ul>
