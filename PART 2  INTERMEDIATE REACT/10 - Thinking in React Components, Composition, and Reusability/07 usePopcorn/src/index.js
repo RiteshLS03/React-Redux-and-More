9aa40913fd693f42c81a34318fb3884558ae1e29
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-02-18 10:55:22 +0530

    Part-2 SEC-10 Completed Day-11

--- PART 2  INTERMEDIATE REACT/10 - Thinking in React Components, Composition, and Reusability/07 usePopcorn/src/index.js
+++ PART 2  INTERMEDIATE REACT/10 - Thinking in React Components, Composition, and Reusability/07 usePopcorn/src/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [moviesRating, setMoviesRating] = useState(0);
+  return (
+    <div>
+      <StarRating color={"blue"} onSetRating={setMoviesRating} />
+      <p>This movie has {moviesRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      color={"red"}
+      size={24}
+      className={""}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} />
+    <Test />
 
     {/* <StarRating
 
