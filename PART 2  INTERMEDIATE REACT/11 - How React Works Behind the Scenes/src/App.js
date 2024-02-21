6fefc3662087dee40d3ef45b18430b946efb2247
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-02-21 21:09:49 +0530

    NULL

--- PART 2  INTERMEDIATE REACT/11 - How React Works Behind the Scenes/src/App.js
+++ PART 2  INTERMEDIATE REACT/11 - How React Works Behind the Scenes/src/App.js
@@ -61,21 +61,43 @@ function Tab({ num, activeTab, onClick }) {
   );
 }
 
-console.log("RENDER");
+// console.log("RENDER");
 
 function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    //
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+
+    handleInc();
+    handleInc();
+    handleInc();
   }
 
   function handleUndo() {
-    setShowDetails(!showDetails);
+    setShowDetails(true);
     setLikes(0);
     console.log(likes);
   }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  console.log("NULL");
 
   return (
     <div className="tab-content">
@@ -90,13 +112,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button onClick={() => handleUndo()}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
